d78ae04408d88faf576e62dca3ee711580b45e96
Urjit Singh Bhatia <urjitsb87@gmail.com>
2018-08-31 06:50:03 +0000

    Add more tests for sshfile including dir ops, reset file seek on readat

--- sshfs/sshfsFile.go
+++ sshfs/sshfsFile.go
@@ -19,12 +19,15 @@ func newSSHFile(f *sftp.File, c *sftp.Client) afero.File {
 	return &file{f, c}
 }
 
+// ReadAt reads reads up to len(b) bytes at offset n
 func (f *file) ReadAt(b []byte, n int64) (int, error) {
 	n, err := f.Seek(n, io.SeekStart)
 	if err != nil {
 		return int(n), err
 	}
 
+	// rewind the file - we don't have access to underlying readAt
+	defer f.Seek(0, io.SeekStart)
 	return f.Read(b)
 }
 
@@ -37,17 +40,9 @@ func (f *file) WriteAt(b []byte, n int64) (int, error) {
 }
 
 func (f *file) Readdir(count int) ([]os.FileInfo, error) {
-	info, err := f.Stat()
-	if err != nil {
-		return nil, err
+	if count < 1 {
+		return nil, errors.New("Readdir count should be greater than 0. Path: " + f.Name())
 	}
-	if !info.IsDir() {
-		return nil, &os.PathError{Err: errors.New("Non dir file"), Op: "readdir", Path: f.Name()}
-	}
-	return f.c.ReadDir(f.Name())
-}
-
-func (f *file) Readdirnames(n int) ([]string, error) {
 	info, err := f.Stat()
 	if err != nil {
 		return nil, err
@@ -59,10 +54,37 @@ func (f *file) Readdirnames(n int) ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
-	// TODO - paginate on n
-	names := make([]string, len(infos))
-	for i, n := range infos {
-		names[i] = n.Name()
+	infosLen := len(infos)
+	outLen := 0
+	switch {
+	case infosLen == 0, infosLen == count:
+		return infos, err
+	case infosLen > count:
+		outLen = count
+	case infosLen < count:
+		outLen = infosLen
+	}
+	return infos[:outLen], err
+}
+
+func (f *file) Readdirnames(count int) ([]string, error) {
+	infos, err := f.Readdir(count)
+	if err != nil {
+		return nil, err
+	}
+	infosLen := len(infos)
+	outLen := 0
+	switch {
+	case infosLen > count:
+		outLen = count
+	case infosLen < count:
+		outLen = infosLen
+	default:
+		outLen = count
+	}
+	names := make([]string, outLen)
+	for i := 0; i < outLen; i++ {
+		names[i] = infos[i].Name()
 	}
 
 	return names, nil
--- sshfs/sshfs_file_test.go
+++ sshfs/sshfs_file_test.go
@@ -52,7 +52,7 @@ func TestReadWrite(t *testing.T) {
 		t.Fatalf("Expected file to contain: 'Hello World golang.org' Found: %s", bufs)
 	}
 
-	// file.ReadAt
+	// file.ReadAt - also that readAt leaves fd pointing back to start
 	buf = make([]byte, 5)       // Read World
 	_, err = rwf.ReadAt(buf, 6) // Skip 'Hello '
 	fatalOnErr(t, errors.WithStack(err))
@@ -60,6 +60,14 @@ func TestReadWrite(t *testing.T) {
 	bufs = string(buf)
 	if bufs != "World" {
 		t.Fatalf("Expected file to contain: 'World' Found: %s", bufs)
+	}
+	// follow ReadAt with a ReadAll to test file is back to start after readAt
+	buf, err = ioutil.ReadAll(rwf)
+	fatalOnErr(t, errors.WithStack(err))
+
+	bufs = string(buf)
+	if bufs != "Hello World golang.org" {
+		t.Fatalf("Expected file to contain: 'Hello World golang.org' Found: %s", bufs)
 	}
 
 	// file.Name
@@ -104,5 +112,74 @@ func TestReadWrite(t *testing.T) {
 	e, _ = err.(*os.PathError)
 	if e.Err.Error() != "Non dir file" {
 		t.Fatal("Expected os.Path performing Readdir on non-dir file", err)
+	}
+}
+
+func TestDirCalls(t *testing.T) {
+	testDirName := "testReadWriteDir"
+	// Get test file system
+	fs := getTestFs(t)
+
+	dir, err := ioutil.TempDir("", "sftptest")
+	fatalOnErr(t, errors.WithStack(err))
+	sub := path.Join(dir, testDirName)
+
+	// Create dir
+	fatalOnErr(t, errors.WithStack((fs.Mkdir(sub, os.ModeDir|os.FileMode(0770)))))
+
+	// Get dir handle
+	d, err := fs.Open(sub)
+	fatalOnErr(t, errors.WithStack(err))
+
+	// Readdir
+	info, err := d.Readdir(10)
+	fatalOnErr(t, errors.WithStack(err))
+	if len(info) != 0 {
+		t.Fatalf("Expected empty dir to return 0 info items on readdir. Got %+v", info)
+	}
+
+	// Readdir with count <= 0 should err
+	_, err = d.Readdir(-1)
+	if err == nil {
+		t.Fatalf("Expected readdir with count 0 to err")
+	}
+
+	// Now create files in the dir
+	_, err = fs.Create(path.Join(sub, "testFile1"))
+	fatalOnErr(t, errors.WithStack(err))
+	_, err = fs.Create(path.Join(sub, "testFile2"))
+	fatalOnErr(t, errors.WithStack(err))
+
+	// Now Readdir should return info about the file we created (count > num files)
+	info, err = d.Readdir(10)
+	fatalOnErr(t, errors.WithStack(err))
+	if len(info) != 2 {
+		t.Fatalf("Expected non-empty dir to return 2 info items on readdir")
+	}
+	if info[0].Name() != "testFile1" {
+		t.Fatalf("Expected readdir to list testFile")
+	}
+
+	// Now Readdir should return info about the file we created (count < num files)
+	info, err = d.Readdir(1)
+	fatalOnErr(t, errors.WithStack(err))
+	if len(info) != 1 {
+		t.Fatalf("Expected non-empty dir to return 1 (count 1) info items on readdir")
+	}
+	if info[0].Name() != "testFile1" {
+		t.Fatalf("Expected readdir to list testFile")
+	}
+
+	// Readdirnames - count > num items
+	names, err := d.Readdirnames(10)
+	fatalOnErr(t, errors.WithStack(err))
+	if len(names) != 2 {
+		t.Fatalf("Expected non-empty dir to return 2 (count 2) name items on readdirnames")
+	}
+	// Readdirnames - count < num items
+	names, err = d.Readdirnames(1)
+	fatalOnErr(t, errors.WithStack(err))
+	if len(names) != 1 {
+		t.Fatalf("Expected non-empty dir to return 1 (count 1) name items on readdirnames")
 	}
 }
